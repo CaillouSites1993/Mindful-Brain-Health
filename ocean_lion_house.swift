a53d0b9d38bbfeeddcff40aa29b6353c79d0a0e2
CaillouSites1993 <137517653+CaillouSites1993@users.noreply.github.com>
2023-06-29 05:53:21 +0700

    Enhance search result relevancy

---
+++ ocean_lion_house.swift
@@ -0,0 +1,61 @@
+import UIKit
+import Foundation
+
+//MARK: - Mindful Brain Health
+
+// Activity 1: Body Scan
+// A body scan is an exercise that helps focus on each part of the body to break down tension and stress. 
+
+func bodyScan() {
+    print("Let's begin with a body scan exercise. Close your eyes and take a deep breath in and out.")
+    print("Now, let's start at the top of your head and work our way down to your toes. Take a few moments to imagine your breath entering and leaving each part of your body and let that tension go.")
+    print("Toes, feet, ankles, lower legs, knees, upper legs, pelvis, lower back, upper back, shoulders, arms, hands, neck, jaw, cheeks, forehead, crown of the head.")
+    print("When you are ready, take a few more deep breaths and open your eyes.")
+    print("Thank you for taking the time for a body scan. That was great.")
+}
+
+// Activity 2: Mindful Breathing
+// Mindful breathing is a type of meditation that focuses on the breath and encourages relaxation.
+
+func mindfulBreathing() {
+    print("Let's start by finding a comfortable position.")
+    print("Now, take a few moments to observe your natural breath.")
+    print("Gently draw your attention to your nose, the sensation of air entering and leaving your body. Allow yourself to become aware of any sensations that arise.")
+    print("When you are ready, start counting your breaths. Breath in for a count of one, then out for a count of two. Repeat this cycle, increasing the counts as you go.")
+    print("Continue doing this until you reach a count of ten. When you are finished, simply observe your breath again. Allow your attention to drift off into a state of peacefulness and relaxation.")
+    print("Thank you for taking the time for a mindful breathing mediation. That was great.")
+}
+
+// Activity 3: Gratitude Meditation
+// Gratitude meditation can help to build a sense of appreciation and contentment.
+
+func gratitudeMeditation() {
+    print("Find a comfortable position and close your eyes.")
+    print("Take a few moments to focus on your breathing. Let your breath flow in and out with ease.")
+    print("Now, start to think of things you are grateful for, big or small. It could be something as simple as the warmth of the sun or the sound of birds chirping.")
+    print("Take a few moments to reflect on each thing and how it brings you joy and happiness. Notice how you feel internally and allow yourself to sink into that feeling of gratitude.")
+    print("When you are ready, take a few more deep breaths and gently open your eyes.")
+    print("Thank you for taking the time for a gratitude meditation. That was great.")
+}
+
+// Activity 4: Mindful Listening
+// Mindful listening is a practice that can help you become more aware of the sounds in your environment and respond better to them.
+
+func mindfulListening() {
+    print("Begin by finding a comfortable and quiet spot to sit.")
+    print("Now, close your eyes and spend a few moments focusing on your breath.")
+    print("Gently open your ears and take notice of the different sounds around you. Listen to each sound carefully and without judgement. Notice how the sound changes and fades away.")
+    print("Continue this practice for a few minutes and when you are ready, take a few more deep breaths and open your eyes.")
+    print("Thank you for taking the time for a mindful listening exercise. That was great.")
+}
+
+// Activity 5: Aromatherapy
+// Aromatherapy is a holistic approach that uses essential oils to support physical and emotional wellbeing.
+
+func aromatherapy() {
+    print("Begin by finding a comfortable place to sit.")
+    print("Now, take a few drops of your favorite essential oil and apply it to your wrists or the back of your neck.")
+    print("Allow the scent to wash over you and deeply inhale its aroma. Notice any sensations that arise as you take each breath.")
+    print("Continue this practice for a few minutes and when you are ready, take a few more deep breaths.")
+    print("Thank you for taking the time for an aromatherapy session. That was great.")
+}
